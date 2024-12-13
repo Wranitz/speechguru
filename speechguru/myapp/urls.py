257029dda713ac622ba25978afd4de97dcdb87a3
Nitesh Paudel <niteshpaudel96@gmail.com>
2024-12-13 17:05:13 +0000

    made endpoints to fetch audio with some exception handelers.

--- speechguru/myapp/urls.py
+++ speechguru/myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("",views.home, name="home"),
     path("todos/",views.todos, name="todos"),
-    path("sathi/",views.sathi, name="sathi")
+    path("sathi/",views.sathi, name="sathi"),
+    path('transcribe/',views.transcribe, name='transcribe'),
 ]
--- speechguru/myapp/views.py
+++ speechguru/myapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from .models import TodoItem
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 def home(request):
@@ -11,3 +14,16 @@ def todos(request):
 
 def sathi(request):
     return render(request, "sathi.html")
+
+@csrf_exempt
+def transcribe(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            #Transcription logic here
+            response_data = {'message': 'Transcription successful'}
+            return JsonResponse(response_data)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+    else:
+        retuen JsonResponse({'error': 'Invalid request method'}, status=405)
